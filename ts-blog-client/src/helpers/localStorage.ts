679f046d690966983ea3640fc5182cac01093e02
elis-11 <eliza.arzanukaeva@gmail.com>
2022-08-14 12:31:14 +0200

    Fixed TSX-LocalStorage

--- ts-blog-client/src/helpers/localStorage.ts
+++ ts-blog-client/src/helpers/localStorage.ts
@@ -1,19 +1,19 @@
-const LOGIN_USER = "LOGIN_USER"
+const LOGIN_USER = "LOGIN_USER";
 
 export const storeUserInLocalStorage = (user: any) => {
-  localStorage.setItem(LOGIN_USER, JSON.stringify(user))
-}
+  localStorage.setItem(LOGIN_USER, JSON.stringify(user));
+};
 
 export const deleteUserInLocalStorage = () => {
-  localStorage.removeItem(LOGIN_USER)
-}
+  localStorage.removeItem(LOGIN_USER);
+};
 
 export const loadUserInLocalStorage = () => {
-  const userStr = localStorage.getItem(LOGIN_USER)
+  const userStr = localStorage.getItem(LOGIN_USER);
 
   // if there is no user in local storage => we are not logged in => return empty user
-  if(!userStr) return
+  if (!userStr) return;
 
-  const userObj = JSON.parse( userStr )
-  return userObj
-}
+  const userObj = JSON.parse(userStr);
+  return userObj;
+};
