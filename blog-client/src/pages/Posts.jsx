abb2627f829c36dad0e92fb53f29cac15ad14d21
elis-11 <eliza.arzanukaeva@gmail.com>
2022-08-28 16:36:42 +0200

    JS - Search-Posts

--- blog-client/src/pages/Posts.jsx
+++ blog-client/src/pages/Posts.jsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PostList } from "../components/posts/PostList";
 import { useDataContext } from "../context/DataProvider";
-import { getPostsApi } from "../helpers/apiCalls";
-import '../components/posts/Posts.scss'
+import { deletePostApi, getPostsApi } from "../helpers/apiCalls";
+import "../components/posts/Posts.scss";
+import { NavLink } from "react-router-dom";
 
 export const Posts = () => {
   const { posts, setPosts, errors, setErrors } = useDataContext();
+  const [search, setSearch] = useState("");
+  const inputRef= useRef();
 
   useEffect(() => {
     const loadData = async () => {
@@ -19,10 +22,40 @@ export const Posts = () => {
     loadData();
   }, []);
 
+  const filteredPosts = posts.filter(
+    (post) =>
+      post.title.toLowerCase().includes(search.toLowerCase()) ||
+      post.author.name.toLowerCase().includes(search.toLowerCase()) ||
+      post.description.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="Posts">
-      <h2>Posts</h2>
-      <PostList />
+      <div className="header">
+        <h3>
+          {posts.length} List {posts.length === 1 ? "post" : "posts"}
+        </h3>
+        <NavLink to="/addpost">Create New Post</NavLink>
+        <div className="search">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <input
+              autoFocus
+              ref={inputRef}
+              id="search"
+              type="text"
+              role="search"
+              placeholder="Search..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </form>
+        </div>
+      </div>
+      <PostList posts={filteredPosts} />
       <div className="errors">{errors}</div>
     </div>
   );
